18c74c5cc52a3856af584e1be9b5e6bebfc5a2e3
Alessandro-Silvestri <103107872+Alessandro-Silvestri@users.noreply.github.com>
2022-04-06 10:35:54 +0100

    Update Bancomat pin simulation - while infinite loop.py

--- Bancomat pin simulation - while infinite loop.py
+++ Bancomat pin simulation - while infinite loop.py
@@ -1,14 +1,14 @@
 # ATM pin simulation
-# I created a function that check if the pin code is correct
+# I created a function that checks if the pin code, insert by an user, is correct
 
 def atm(pin):
     while True:                                             # I create an INFINITE loop
-        user_pin_answer = int(input('insert pin: '))
+        user_pin_answer = int(input('insert pin: '))        # I convert in integer value
         if user_pin_answer == pin:
             print('OK! You are inside your Bank account')
             break                                           # I get out from the INFINITE loop
         else:
             print('pin wrong, retry')
 
-# I recall the function and I pass it a value (pin)
+# I recall the function and I pass it the secret pin
 atm(5678)
