2c1eac5d218dc803e16419e40983a713324da618
Alessandro-Silvestri <103107872+Alessandro-Silvestri@users.noreply.github.com>
2022-04-07 22:46:00 +0100

    Add files via upload

---
+++ random dices - class.py
@@ -0,0 +1,16 @@
+import random
+
+
+class Dice:
+    def roll(self):                 # I define a function in a class
+        a = random.randint(1, 6)
+        b = random.randint(1, 6)
+        return a, b                 # Python puts 'a' and 'b' in a tuple automatically
+
+
+dice = Dice()                       # I create an object from the class Dice
+print(dice.roll())                  # I call the function 'roll' from the class 'Dice'
+
+
+
+
