85a6f4c918795ef7d2c37c1d9da642995c35e62e
Alessandro-Silvestri <103107872+Alessandro-Silvestri@users.noreply.github.com>
2022-08-15 23:29:38 +0100

    Add files via upload

---
+++ file_text_letters_counter.py
@@ -0,0 +1,34 @@
+# TEXT FILE LETTERS COUNTER
+# Made by Alessandro Silvestri for Python Institute
+
+# user interface: loop until the file name is correct
+while True:
+     try:
+          input_file_name = input('\nCounting letters in alphabetical order, insert file name: ')
+          break
+     except:
+          print('error file')
+
+letters_dict = {}
+reader = open(input_file_name, 'r')
+content = reader.read().lower()
+
+# letters counting
+for i in content:
+     if i in letters_dict:
+          letters_dict[i] += 1
+     elif i == ' ' or i == '.' or i == '\n':
+          continue
+     else:
+          letters_dict[i] = 1
+
+# creating a letters_dict keys list in alphabetical order
+letters_list =[i for i in letters_dict.keys()]
+letters_list.sort()
+
+# print the histogram
+for i in letters_list:
+     print(f'{i} -> {letters_dict[i]}')
+
+reader.close()
+
