8fd60bf389e36db3a2bffffbb3b0c05c94623d81
Alessandro-Silvestri <103107872+Alessandro-Silvestri@users.noreply.github.com>
2022-08-16 12:59:56 +0100

    Update file_text_letters_counter.py

--- file_text_letters_counter.py
+++ file_text_letters_counter.py
@@ -5,13 +5,15 @@
 while True:
      try:
           input_file_name = input('\nCounting letters in alphabetical order, insert file name: ')
+          letters_dict = {}
+          reader = open(input_file_name, 'r')
+          content = reader.read().lower()
+          reader.close()
           break
      except:
           print('error file')
 
-letters_dict = {}
-reader = open(input_file_name, 'r')
-content = reader.read().lower()
+
 
 # letters counting
 for i in content:
@@ -29,6 +31,4 @@ letters_list.sort()
 # print the histogram
 for i in letters_list:
      print(f'{i} -> {letters_dict[i]}')
-
-reader.close()
 
