ed6e4256bdf850408d1b60784d4a1f48d3664a64
Alessandro-Silvestri <103107872+Alessandro-Silvestri@users.noreply.github.com>
2022-08-17 22:46:52 +0100

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,30 @@
+# PASSWORD GENERATOR
+# This program generates a string with random characters capital/no capital, numbers and 
+# the first character can't be a number.
+# In the function you can pass the password lenght.
+# Made by Alessandro Silvestri
+
+import random
+
+def password_generator(chrnum):
+     # The first character has to be a letter: capital or not
+     if random.randint(0, 1) == 0:
+          password = chr(random.randint(65, 90))
+     else:
+          password = chr(random.randint(97, 122))
+
+     # password generator loop
+     character = ''
+     counter = 1
+     while counter <= chrnum - 1:
+          character = chr(random.randint(48, 122))
+          if not character.isalnum():
+               continue
+          password += character
+          counter += 1
+     return password
+
+# in the function I pass the password lenght
+print(password_generator(20))
+
+
